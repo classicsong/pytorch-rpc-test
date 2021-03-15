a8c920ebf03f63129fb0560060861df0cf1289f8
Song <classicxsong@gmail.com>
2021-03-15 12:22:32 +0800

    end2end with slice

--- async_rpc_test.py
+++ async_rpc_test.py
@@ -1,46 +1,62 @@
 import torch as th
+import torch.nn as nn
 import argparse
 import time
 
 import torch.distributed.rpc as rpc
 
-@th.jit.script
-def collect(data: th.Tensor, input : int) -> th.Tensor:
-    return data
+class DataServer(nn.Module):
+    def __init__(self, data, i):
+       super(DataServer, self).__init__()
+       self.data = data
 
-def run(arg, data):
-    name = arg.name
-    remote = arg.remote
-    rpc.init_rpc(name,
-                 rank=arg.rank,
-                 world_size=arg.world_size,
-                 rpc_backend_options=rpc.TensorPipeRpcBackendOptions(num_worker_threads=args.workers))
-    result = rpc.rpc_sync(remote, collect, args=(data, 2))
-    async_result = []
-    t1 = time.time()
-    for i in range(args.workers):
-        async_result.append(rpc.rpc_async(remote, collect, args=(data, 2)))
+    def forward(self, idx):
+        return self.data[idx]
 
-    for i in range(args.workers):
-        async_result[i].wait()
-    t2 = time.time()
-    print('RTT of {} tensor is {}'.format(result.shape, t2-t1))
-    rpc.shutdown()
+class Server():
+    def __init__(self, args, data):
+        self.data = data
+        self.run(args)
 
-def start(args):
-    rpc.init_rpc(args.name,
-                 rank=args.rank,
-                 world_size=args.world_size,
-                 rpc_backend_options=rpc.TensorPipeRpcBackendOptions(num_worker_threads=args.workers))
+    def run(self, args):
+        if args.rank == 0:
+            self.run_client(args)
+        else:
+            self.run_server(args)
 
-    rpc.shutdown()
+    def run_client(self, arg):
+        name = arg.name
+        remote = arg.remote
+        rpc.init_rpc(name, rank=arg.rank, world_size=arg.world_size,
+                     rpc_backend_options=rpc.TensorPipeRpcBackendOptions(num_worker_threads=args.workers))
+                     #rpc_backend_options=rpc.ProcessGroupRpcBackendOptions(num_send_recv_threads=args.workers))
+        rref = rpc.remote(remote, DataServer, args=(self.data, 1))
+        randidx = th.randint(self.data.shape[0], (10000,))
+        result = rref.rpc_sync().forward(randidx)
+
+        randidx = th.randint(self.data.shape[0], (10000,))
+        async_result = []
+        t1 = time.time()
+        for i in range(args.workers):
+            async_result.append(rref.rpc_async().forward(randidx))
+        th.futures.wait_all(async_result)
+        t2 = time.time()
+        print('RTT of {} tensor is {}'.format(result.shape, t2-t1))
+        rpc.shutdown()
+
+    def run_server(self, args):
+        rpc.init_rpc(args.name, rank=args.rank, world_size=args.world_size,
+                     rpc_backend_options=rpc.TensorPipeRpcBackendOptions(num_worker_threads=args.workers))
+                     #rpc_backend_options=rpc.ProcessGroupRpcBackendOptions(num_send_recv_threads=args.workers))
+
+        rpc.shutdown()
 
 
 class ArgParser(argparse.ArgumentParser):
     def __init__(self):
         super(ArgParser, self).__init__()
 
-        self.add_argument('--size', type=int, default=1024 * 1024)
+        self.add_argument('--size', type=int, default=100 * 1024)
         self.add_argument('--world_size', type=int, default=2)
         self.add_argument('--rank', type=int, default=0)
         self.add_argument('--name', type=str)
@@ -50,8 +66,6 @@ class ArgParser(argparse.ArgumentParser):
 if __name__ == '__main__':
     args = ArgParser().parse_args()
 
-    data = th.empty((args.size, 128))
-    if args.rank == 0:
-        run(args, data)
-    else:
-        start(args)
+    data = th.rand((args.size, 10))
+    server = Server(args, data)
+    
--- rpc_test.py
+++ rpc_test.py
@@ -1,46 +1,65 @@
 import torch as th
+import torch.nn as nn
 import argparse
 import time
 
 import torch.distributed.rpc as rpc
 
-@th.jit.script
-def collect(data: th.Tensor, input : int) -> th.Tensor:
-    return data
+class DataServer(nn.Module):
+    def __init__(self, data, i):
+       super(DataServer, self).__init__()
+       self.data = data
 
-def run(arg, data):
-    name = arg.name
-    remote = arg.remote
-    rpc.init_rpc(name, rank=arg.rank, world_size=arg.world_size)
-    result = rpc.rpc_sync(remote, collect, args=(data, 2))
-    t1 = time.time()
-    result = rpc.rpc_sync(remote, collect, args=(data, 2))
-    t2 = time.time()
-    print('RTT of {} tensor is {}'.format(result.shape, t2-t1))
-    rpc.shutdown()
+    def forward(self, idx):
+        return self.data[idx]
 
-def start(args):
-    rpc.init_rpc(args.name, rank=args.rank, world_size=args.world_size, num_worker_threads=8)
+class Server():
+    def __init__(self, args, data):
+        self.data = data
+        self.run(args)
 
-    rpc.shutdown()
+    def run(self, args):
+        if args.rank == 0:
+            self.run_client(args)
+        else:
+            self.run_server(args)
+
+    def run_client(self, arg):
+        name = arg.name
+        remote = arg.remote
+        rpc.init_rpc(name, rank=arg.rank, world_size=arg.world_size,
+                     rpc_backend_options=rpc.TensorPipeRpcBackendOptions())
+                     #rpc_backend_options=rpc.ProcessGroupRpcBackendOptions())
+        rref = rpc.remote(remote, DataServer, args=(self.data, 1))
+        randidx = th.randint(self.data.shape[0], (10000,))
+        result = rref.rpc_sync().forward(randidx)
+
+        randidx = th.randint(self.data.shape[0], (10000,))
+        t1 = time.time()
+        result = rref.rpc_sync().forward(randidx)
+        t2 = time.time()
+        print('RTT of {} tensor is {}'.format(result.shape, t2-t1))
+        rpc.shutdown()
+
+    def run_server(self, args):
+        rpc.init_rpc(args.name, rank=args.rank, world_size=args.world_size,
+                     rpc_backend_options=rpc.ProcessGroupRpcBackendOptions())
+
+        rpc.shutdown()
 
 
 class ArgParser(argparse.ArgumentParser):
     def __init__(self):
         super(ArgParser, self).__init__()
 
-        self.add_argument('--size', type=int, default=1024 * 1024)
+        self.add_argument('--size', type=int, default=100 * 1024)
         self.add_argument('--world_size', type=int, default=2)
         self.add_argument('--rank', type=int, default=0)
         self.add_argument('--name', type=str)
         self.add_argument('--remote', type=str)
-        self.add_argument('--async', type=bool)
 
 if __name__ == '__main__':
     args = ArgParser().parse_args()
 
-    data = th.empty((args.size, 128))
-    if args.rank == 0:
-        run(args, data)
-    else:
-        start(args)
+    data = th.rand((args.size, 10))
+    server = Server(args, data)
